5163e08a5c4a42ee1fbbdf69d01af68364e41832
parker <parker.douglass@icloud.com>
2021-10-13 21:24:11 -0400

    Continued to make improvements to the landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,26 @@
 <template>
   <v-app>
+    <!-- 
+      Below is the top navigation bar. Will integrate mobile
+      view later
+     -->
     <v-app-bar 
       class="navbar"
       elevate-on-scroll 
       flat
       color = "white"
     >
-      <v-app-bar-title>
+      <v-app-bar-title><!-- app-bar-title is the left text -->
         <span class="nav-title">TCT</span>
       </v-app-bar-title>
-      <v-spacer></v-spacer>
+      <v-spacer></v-spacer><!-- Middle -->
       <div class="nav-link-container">
       <nuxt-link to="/" class="nav-link">Home</nuxt-link>
       <nuxt-link to="/" class="nav-link">About</nuxt-link>
       <nuxt-link to="/" class="nav-link">Episodes</nuxt-link>
       <nuxt-link to="/" class="nav-link">Contact us</nuxt-link>
       </div>
-      <v-spacer></v-spacer>
+      <v-spacer></v-spacer> <!-- Right -->
       <div class="login-button-container">
       <nuxt-link to="/" class="login-button">Login</nuxt-link>
       </div>
@@ -30,7 +34,10 @@
         <Nuxt />
       </v-content>
     </v-main>
-    <!-- Footer goes here -->
+    <!-- 
+      Footer will eventually go here. This is one of the
+      last priorities though
+     -->
   </v-app>
 </template>
 <!-- Below is the styling for everything in here -->
@@ -40,6 +47,9 @@
     flex: 0.1;
   }
   .navbar{
+    .nav-title{
+      text-overflow: unset;
+    }
     .nav-link{
       text-decoration: none;
       color: black;
